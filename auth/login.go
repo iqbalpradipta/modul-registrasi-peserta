1eec15abcb4be21b859d24b49b20ee6fdb3d77f3
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-03-12 13:27:57 +0700

    update

--- auth/login.go
+++ auth/login.go
@@ -1,12 +1,28 @@
 package auth
 
 import (
+	"errors"
+
+	"github.com/iqbalpradipta/modul-registrasi-peserta/hash"
 	"github.com/iqbalpradipta/modul-registrasi-peserta/models"
+	"github.com/iqbalpradipta/modul-registrasi-peserta/middlewares"
 )
 
 
-UserNF := errors.New("User not found !")
+var ErrUserNF = errors.New("user not found")
 
 func SignIn(email, password string) (string, error) {
-	user := models.Get	
+	user := models.GetUserByEmail(email)
+	if user.Id == 0 {
+		return "", ErrUserNF
+	}
+	err := hash.PswdVerif(user.Password, password)
+	if err != nil {
+		return "", nil
+	}
+	token, err := middlewares.GenerateJWT(user)
+	if err != nil {
+		return "", err 
+	}
+	return token, nil
 }
