11501e42b5eeda592334a2e57ccf2db09f0f3711
iqbalpradipta <iqbalpradipta2@gmail.com>
2023-03-12 23:00:45 +0700

    fix

--- config/database.go
+++ config/database.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"fmt"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -18,6 +20,7 @@ func InitDB() *gorm.DB {
 	URL := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME)
 	db, err := gorm.Open("postgres", URL)
 	if err != nil {
+		log.Fatal(err)
 		return nil
 	}
 	return db
